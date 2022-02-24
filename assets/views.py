f4cfc134fe4780767de0d43ecfda7c8ab54f4dd8
D-Lawson <dan.lawson@live.co.uk>
2022-02-24 00:41:28 +0000

    Add views for search

--- assets/views.py
+++ assets/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Asset
 
 
@@ -6,9 +8,21 @@ def all_assets(request):
     """ Displays all assets with sorting and searching features """
 
     assets = Asset.objects.all()
+    query = None
+
+    if request.GET:
+        if 'search' in request.GET:
+            query = request.GET['search']
+            if not query:
+                messages.error(request, "Please enter a search criteria")
+                return redirect(reverse('assets'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            assets = assets.filter(queries)
 
     context = {
         'assets': assets,
+        'search_criteria': query,
     }
 
     return render(request, 'assets/assets.html', context)
