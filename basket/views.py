418839442bafbf9332d9fcbfc906d561becdaf1b
D-Lawson <dan.lawson@live.co.uk>
2022-03-06 15:18:10 +0000

    Add functionality for adding to basket

--- basket/views.py
+++ basket/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
+
 def view_basket(request):
-    """ Reners the basket template with its contents """
+    """ Renders the basket template with its contents """
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, asset_id):
+    """ Adds asset to basket """
+
+    quantity = 1
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    basket[asset_id] = quantity
+
+    request.session['basket'] = basket
+    return redirect(redirect_url)
