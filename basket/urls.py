58be1cac4e16d948e08ca8e7abcc3a04dfbdfc43
D-Lawson <dan.lawson@live.co.uk>
2022-03-10 00:28:41 +0000

    Add basket functionality for remove and subtotal

--- basket/urls.py
+++ basket/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
-    path('add/<asset_id>', views.add_to_basket, name='add_to_basket')
+    path('add/<asset_id>/', views.add_to_basket, name='add_to_basket'),
+    path('update/<asset_id>/', views.update_basket, name='update_basket'),
+    path('remove/<asset_id>/', views.remove_asset, name='remove_asset'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -37,3 +37,56 @@ def add_to_basket(request, asset_id):
     request.session['basket'] = basket
     print(request.session['basket'])
     return redirect(redirect_url)
+
+
+def update_basket(request, asset_id):
+    """ Update quantity within basket """
+
+    quantity = int(request.POST.get('quantity'))
+
+    size = None
+    if 'print_size' in request.POST:
+        size = request.POST['print_size']
+
+    basket = request.session.get('basket', {})
+
+    if size:
+        if quantity > 0:
+            basket[asset_id]['asset_by_size'][size] = quantity
+        else:
+            del basket[asset_id]['asset_by_size'][size]
+            if not basket[asset_id]['asset_by_size']:
+                basket.pop(asset_id)
+    else:
+        if quantity > 0:
+            basket[asset_id] = quantity
+        else:
+            basket.pop(asset_id)
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return redirect(reverse('view_basket'))
+
+
+def remove_asset(request, asset_id):
+    """ Remove asset from basket """
+
+    try:
+        size = None
+        if 'print_size' in request.POST:
+            size = request.POST['print_size']
+
+        basket = request.session.get('basket', {})
+
+        if size:
+            del basket[asset_id]['asset_by_size'][size]
+            if not basket[asset_id]['asset_by_size']:
+                basket.pop(asset_id)
+        else:
+            basket.pop(asset_id)
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+        
+    except Exception as e:
+        return HttpResponse(status=500)
