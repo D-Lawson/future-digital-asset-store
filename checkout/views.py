bf8bbcd1e64d9f2bc70d6b7b0beea54b7d550803
D-Lawson <dan.lawson@live.co.uk>
2022-03-29 21:41:42 +0000

    Render stripe element and adjust checkout form styling

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KiQsPLL0Ptw5kQUD9xA84jUktDG65tIKCelRBQcyPvPfa6ocMw2nZrc3DbwihcwsyXM6knEigis337bdJQTeQfP004rP8ZZMf',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
