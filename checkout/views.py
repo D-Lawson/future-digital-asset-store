56c07b33e27eb85f3d7deff1d028ef267f50d211
D-Lawson <dan.lawson@live.co.uk>
2022-03-29 23:25:15 +0000

    Add stripe functionality to checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,22 +1,41 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
+import stripe
+from basket.contexts import basket_contents
 from .forms import OrderForm
 
 
 def checkout(request):
     """ View for rendering to the checkout template """
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "There's nothing in your basket at the moment")
         return redirect(reverse('assets'))
 
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing from \
+            environment')
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KiQsPLL0Ptw5kQUD9xA84jUktDG65tIKCelRBQcyPvPfa6ocMw2nZrc3DbwihcwsyXM6knEigis337bdJQTeQfP004rP8ZZMf',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
--- dig_asset_store/settings.py
+++ dig_asset_store/settings.py
@@ -167,3 +167,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DISCOUNT_THRESHOLD = 25
+
+# Stripe
+
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
