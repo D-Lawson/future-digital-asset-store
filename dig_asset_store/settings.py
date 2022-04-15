b05cc528629908f9e4dbe3fe4bc86c8c5a02406b
D-Lawson <dan.lawson@live.co.uk>
2022-04-15 22:46:49 +0000

    Set secret key to os and set debug

--- dig_asset_store/settings.py
+++ dig_asset_store/settings.py
@@ -9,10 +9,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-13bh%p%#=-%z%zkl1ri6y972v6vp5&&4h_4fo-_k7(rq7=@m%r'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['future-digital-asset-store.herokuapp.com', 'localhost']
 
