c8f7480b9d7297b2fe6df7578ec04dbc7a35ef07
D-Lawson <dan.lawson@live.co.uk>
2022-04-15 21:30:22 +0000

    Deployment configuration

--- assets/forms.py
+++ assets/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Asset, Category
 
 
@@ -12,6 +13,8 @@ class AssetForm(forms.ModelForm):
         """
         model = Asset
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
