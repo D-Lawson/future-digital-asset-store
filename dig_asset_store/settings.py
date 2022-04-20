b90e3e8e91c4489b8391c47804e8a295267421da
D-Lawson <dan.lawson@live.co.uk>
2022-04-20 20:36:52 +0000

    Update settings.py

--- dig_asset_store/settings.py
+++ dig_asset_store/settings.py
@@ -12,8 +12,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['future-digital-asset-store.herokuapp.com', 'localhost']
 
